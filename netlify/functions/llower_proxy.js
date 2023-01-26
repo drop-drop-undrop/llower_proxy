c4df5691b5ec9f6f3b517e5fe6aa15148823313b
drop-drop-undrop <dropdropundrop@gmail.com>
2023-01-26 08:55:49 +0700

    acquire url from path

--- netlify/functions/llower_proxy.js
+++ netlify/functions/llower_proxy.js
@@ -2,29 +2,25 @@ import fetch from 'node-fetch'
 
 exports.handler = async function (event, context) {
   try {
-    console.log(event)
+    const matcher = /.*llower_prox(y\/|y)/
+    const address = event.path.replace(matcher, '')
+    const url = new URL(address)
+    const response = await fetch(url, {
+      method: event.httpMethod,
+      headers: event.headers,
+      body: event.body
+    })
 
-    // const response = await fetch(ENDPOINT, {
-    //   method: event.httpMethod,
-    //   headers: event.headers,
-    //   body: event.body
-    // })
-
-    // return {
-    //   statusCode: response.status,
-    //   headers: { "Access-Control-Allow-Origin": "*" },
-    //   body: await response.text()
-    // }
     return {
-      statusCode: 200,
+      statusCode: response.status,
       headers: { "Access-Control-Allow-Origin": "*" },
-      body: JSON.stringify(event)
-    } 
+      body: await response.text()
+    }
   } catch (error) {
     return {
       statusCode: 400,
       headers: { "Access-Control-Allow-Origin": "*" },
-      body: error.message
+      body: error.message.toLowerCase()
     }
   }
 }
