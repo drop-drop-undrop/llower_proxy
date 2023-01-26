75143e9678a32bc4f5310702c5c1e4c74a492ef1
drop-drop-undrop <dropdropundrop@gmail.com>
2023-01-26 08:35:02 +0700

    echo and log event

--- netlify/functions/llower_proxy.js
+++ netlify/functions/llower_proxy.js
@@ -1,29 +1,25 @@
 import fetch from 'node-fetch'
 
-const ENDPOINT = 'https://api.twitter.com/2/oauth2/token'
-
 exports.handler = async function (event, context) {
   try {
-    const method = event.httpMethod
-    const contentType = event.headers['content-type']
+    console.log(event)
 
-    if (method !== 'POST') {
-      throw Error('unsupported method')
-    } else if (contentType !== 'application/x-www-form-urlencoded') {
-      throw Error('unsupported content type')
-    } else {
-      const response = await fetch(ENDPOINT, {
-        method: method,
-        headers: { 'content-type': contentType },
-        body: event.body
-      })
+    // const response = await fetch(ENDPOINT, {
+    //   method: event.httpMethod,
+    //   headers: event.headers,
+    //   body: event.body
+    // })
 
-      return {
-        statusCode: response.status,
-        headers: { "Access-Control-Allow-Origin": "*" },
-        body: await response.text()
-      }
-    }
+    // return {
+    //   statusCode: response.status,
+    //   headers: { "Access-Control-Allow-Origin": "*" },
+    //   body: await response.text()
+    // }
+    return {
+      statusCode: 200,
+      headers: { "Access-Control-Allow-Origin": "*" },
+      body: event
+    } 
   } catch (error) {
     return {
       statusCode: 400,
